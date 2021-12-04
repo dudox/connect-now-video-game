9d402924ab00129013fbab3602dfaf423bba29e9
dudox <trybemark@gmail.com>
2021-12-04 01:01:28 +0100

    remove chrome redux logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,21 +8,20 @@ import { loadState } from "./localstorage";
 
 const LoggerMiddleware = createLogger();
 const persistedState = loadState();
-const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose;
+// const composeEnhancers =
+//   typeof window === 'object' &&
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//       // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+//     }) : compose;
 
-const enhancer = composeEnhancers(
-    applyMiddleware( ThunkMiddleware),  // other store enhancers if any
-);
+// const enhancer = composeEnhancers(
+//     applyMiddleware( ThunkMiddleware),  // other store enhancers if any
+// );
 const store = createStore(
   rootReducer,
   persistedState,
-  enhancer
-
+  applyMiddleware(LoggerMiddleware, ThunkMiddleware),
 );
 
 export default store;
